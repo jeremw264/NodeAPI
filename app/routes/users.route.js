8f870d0719f015898b24550e6a1f30d6f5ec630e
Jérémy Woirhaye <jerem.woirhaye@gmail.com>
2022-06-04 17:58:11 +0200

    <maj> Mise à jour des routes utilisateur

--- app/routes/users.route.js
+++ app/routes/users.route.js
@@ -2,13 +2,20 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/users.controller");
 const auth = require("../middleware/auth.middleware");
-const Roles = require("../utils/roles.utils")
+const Roles = require("../utils/roles.utils");
 
-router.get("/", auth(Roles.Admin,Roles.User), userController.getAllUsers);
+// methods GET
+router.get("/", auth(Roles.Admin, Roles.User), userController.getAllUsers);
+router.get("/:id", auth(Roles.Admin, Roles.User), userController.getUserById);
+
+// methods POST
 router.post("/", userController.createUser);
-router.put("/", auth(Roles.User), userController.updateUser);
-router.delete("/", userController.deleteUser);
-
 router.post("/login", userController.userLogin);
+
+// methods PUT
+router.put("/", auth(Roles.User), userController.updateUser);
+
+// methods DELETE
+router.delete("/:id", auth(Roles.Admin), userController.deleteUserById);
 
 module.exports = router;
